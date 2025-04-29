43e596c0e78566b5ca850e16073c1a35e726dc48
kristo.jeremy <kristo.jeremy@ui.ac.id>
2025-04-29 15:24:48 +0700

    chore: schedule time

--- src/scheduler.py
+++ src/scheduler.py
@@ -43,8 +43,8 @@ def run_script(script_path):
 
 
 # Schedule tasks (run once a day)
-schedule.every().day.at("10:00").do(run_script, script_path=FIRST_SCRIPT)
-schedule.every().day.at("10:01").do(run_script, script_path=SECOND_SCRIPT)
+schedule.every().day.at("15:22").do(run_script, script_path=FIRST_SCRIPT)
+schedule.every().day.at("15:23").do(run_script, script_path=SECOND_SCRIPT)
 
 logging.info("Scheduler started. Waiting for the next scheduled job...")
 
